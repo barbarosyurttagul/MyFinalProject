5fb5ec6cda518713d63ec9a595a9a42e6609d72b
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2021-03-16 00:57:53 +0300

    12.Gün Ders Sonu

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(product => product.ProductName).NotEmpty();
+            RuleFor(product => product.ProductName).MinimumLength(2);
+            RuleFor(product => product.UnitPrice).NotEmpty();
+            RuleFor(product => product.UnitPrice).GreaterThanOrEqualTo(0).When(product => product.CategoryId == 1);
+            RuleFor(product => product.ProductName).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return (arg.StartsWith("A")); 
+                 
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            //ProductValidator validator = new ProductValidator();
+            var context = new ValidationContext<object>(entity);
+            ValidationResult result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
